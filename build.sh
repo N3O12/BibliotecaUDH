63b8fd1b19ceadbe988c9b937d278844d90ffdd4
N3O <109119318+N3O12@users.noreply.github.com>
2024-11-05 06:50:00 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
-python manage.py makemigrations
+#python manage.py makemigrations
 # Apply any outstanding database migrations
-python manage.py migrate
+#python manage.py migrate
